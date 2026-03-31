e09044297bd951c7c4a4fcb3cecd8f3f018ed481
Krutarth-02 <126319578+Krutarth-02@users.noreply.github.com>
2026-03-31 09:32:37 +0530

    User Schema fixes

--- clothshopbackend/models/User.js
+++ clothshopbackend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema({
     email:{
-        type:SVGStringList,
+        type:String,
         require:true,
         unique:true,
     },
