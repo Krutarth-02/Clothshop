864158c6bc2160adb64d0eaa2ac79db5242f13ff
Krutarth-02 <126319578+Krutarth-02@users.noreply.github.com>
2026-04-12 13:46:25 +0530

    User Model Fix

--- clothshopbackend/models/User.js
+++ clothshopbackend/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
+    fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    fullName: { type: String, required: true },
     city: { type: String },
     country: { type: String },
     phone: { type: String },
